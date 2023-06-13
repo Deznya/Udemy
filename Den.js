7ad181a6b3ea9d40efe2dbd5486f963f811dcf7e
NYVCHYCK DENYS <109597752+Deznya@users.noreply.github.com>
2023-06-13 15:26:43 +0300

    Update Den.js
    Update salary task condition

--- Den.js
+++ Den.js
@@ -151,6 +151,46 @@ function lookUpProfile(name, prop) {
 
 console.log(lookUpProfile('Akira', 'likes'))
 
+// Задача по критериям подобная
+
+//let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//         age: 28,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//         age: 29,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//         age: 30,
+//     },
+//     {
+//         name: 'name4',
+//         salary: 600,
+//         age: 31,
+//     },
+//     {
+//         name: 'name5',
+//         salary: 700,
+//         age: 32,
+//     },
+// ];
+
+// let totalSalary = 0;
+
+// for (let i = 0; i < employees.length; i++) {
+//     if (employees[i].age >= 30) {
+//         totalSalary += employees[i].salary;
+//     }
+// }
+
+// console.log('Сумма зарплат работников возрастом 30 и более лет:', totalSalary);
+
 
 
 
